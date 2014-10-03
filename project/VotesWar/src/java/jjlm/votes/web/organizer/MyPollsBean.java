40e2259785840126d0c54442dabf59ce9d27a0a0
deviljs <maxmeffert@uni-koblenz.de>
2014-10-03 19:21:13 +0200

    save of local changes

--- project/VotesWar/src/java/jjlm/votes/web/organizer/MyPollsBean.java
+++ project/VotesWar/src/java/jjlm/votes/web/organizer/MyPollsBean.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import jjlm.votes.web.Poll;
+import jjlm.votes.web.data.Poll;
 
 /**
  *
