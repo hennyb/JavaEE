a4a93c219ea1d2e2b498371e99db0047d9462985
deviljs <maxmeffert@uni-koblenz.de>
2014-10-18 00:31:30 +0200

    created ejb architecture diagrams

--- project/VotesWar/src/java/jjlm/votes/web/organizer/EditPollBean.java
+++ project/VotesWar/src/java/jjlm/votes/web/organizer/EditPollBean.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
